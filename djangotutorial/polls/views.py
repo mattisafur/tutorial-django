88290285fe176e1f1a6505818b3c3d0359fb1096
mattisafur <46404761+mattisafur@users.noreply.github.com>
2024-10-27 16:23:13 +0200

    raised 404 error
    https://docs.djangoproject.com/en/5.1/intro/tutorial03/#raising-a-404-error

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from models import Question
 
@@ -11,7 +11,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
